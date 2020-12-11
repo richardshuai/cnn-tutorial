2cb84c3e456691a577487c0bf53beeca1caf54c4
Richard <richardshuai98@gmail.com>
2020-12-10 23:20:45 -0800

    Add changes from peer review

--- common_utils.py
+++ common_utils.py
@@ -3,18 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 
-def plot_labeled_image_grid(images, labels, title=None):
+def plot_labeled_image_grid(images, labels, title=None, cmap=None, sizes=2.5, n_cols=10):
     N = images.shape[0]
-    
-    n_rows = int(np.ceil(N/10))
-    n_cols = 10
-    
-    fig = plt.figure(figsize=(2.5*n_cols, 2.5*n_rows))
-    
+
+    n_rows = int(np.ceil(N/n_cols))
+
+    fig = plt.figure(figsize=(sizes*n_cols, sizes*n_rows))
+
     # Construct a grid to plot images
     grid = ImageGrid(fig, 111,
              nrows_ncols=(n_rows, n_cols),
-             axes_pad=(0.05, 0.5), 
+             axes_pad=(0.05, 0.5),
              )
 
     for i in range(N):
@@ -24,27 +23,29 @@ def plot_labeled_image_grid(images, labels, title=None):
         ax.get_xaxis().set_ticks([])
         label = labels[i]
         ax.set_title('{}'.format(label))
-        ax.imshow(im)
+        if cmap:
+            ax.imshow(im, cmap=cmap)
+        else:
+            ax.imshow(im)
 
     # Remove empty plots
     for i in range(N, n_rows*n_cols):
         grid[i].remove()
-        
+
     plt.suptitle(title, fontsize=16, y=1.01)
     plt.show()
-    
-def plot_image_grid(images, title=None, cmap='gray'):
+
+def plot_image_grid(images, title=None, cmap='gray', sizes=2, n_cols=10):
     N = images.shape[0]
-    
-    n_rows = int(np.ceil(N/10))
-    n_cols = 10
-    
-    fig = plt.figure(figsize=(2.*n_cols, 2*n_rows))
-    
+
+    n_rows = int(np.ceil(N/n_cols))
+
+    fig = plt.figure(figsize=(sizes*n_cols, sizes*n_rows))
+
     # Construct a grid to plot images
     grid = ImageGrid(fig, 111,
                      nrows_ncols=(n_rows, n_cols),
-                     axes_pad=(0.05, 0.05), 
+                     axes_pad=(0.05, 0.05),
                      )
 
     for i in range(N):
@@ -53,10 +54,10 @@ def plot_image_grid(images, title=None, cmap='gray'):
         ax.get_yaxis().set_ticks([])
         ax.get_xaxis().set_ticks([])
         ax.imshow(im, cmap=cmap)
-        
+
     # Remove empty plots
     for i in range(N, n_rows*n_cols):
         grid[i].remove()
-        
+
     plt.suptitle(title, fontsize=16, y=0.92)
-    plt.show()    
+    plt.show()
